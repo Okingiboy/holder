06e4ec46e55454c7c0c7775bbab49898078735c9
“jameschukwu“ <okoriechukwu@yahoo.com>
2022-05-08 18:02:10 -0500

    updated files

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: 'Holder uses SVG to render image placeholders entirely in browser.',
     version: '2.9.9',
-    name: 'imsky:holder',
+    name: 'okingiboy:holder',
     git: 'https://github.com/Okingiboy/holder.git',
 });
 
